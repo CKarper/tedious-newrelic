01f0a286e777aa4a8d376bd6fa2ec3fc80de790d
Christopher Karper <ckarper@underarmour.com>
2018-09-13 18:22:03 -0400

    Make params more meaningful, Add transactions

--- index.js
+++ index.js
@@ -12,14 +12,21 @@ function instrumentTedious(shim, tedious, moduleName) {
 			'makeRequest',
 			{ query: tediousRequestQuery, callback: tediousRequestCallback }
 		);
+
+		shim.recordOperation(proto, ['beginTransaction', 'commitTransaction'], { callback: shim.FIRST });
 	}
 }
 
-function tediousRequestQuery(shim, func, name, [request]) {
-	return args.sqlTextOrProcedure;
+function tediousRequestQuery(shim, func, name, args) {
+	const request = args[0];
+	if (request.parametersByName.statement) {
+		return request.parametersByName.statement.value;
+	}
+	return request.sqlTextOrProcedure;
 }
 
-function tediousRequestCallback(shim, opFunc, opName, segment, [request]) {
+function tediousRequestCallback(shim, opFunc, opName, segment, args) {
+	const request = args[0];
 	request.callback = shim.bindSegment(request.callback, segment);
 }
 
