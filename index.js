62a08e4ce4f1b8d8b261beef9aa06d632915e515
Christopher Karper <ckarper@underarmour.com>
2019-05-14 17:01:50 -0400

    Lint compliance

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ const newrelic = require('newrelic');
 
 const MSSQL = 'MSSQL';
 
-function instrumentTedious(shim, tedious, moduleName) {
+function tediousRequestQuery(shim, func, name, args) {
+	const request = args[0];
+	if (request.parametersByName && request.parametersByName.statement) {
+		return request.parametersByName.statement.value;
+	}
+	return request.sqlTextOrProcedure;
+}
+
+function tediousRequestCallback(shim, opFunc, opName, segment, args) {
+	const request = args[0];
+	request.callback = shim.bindSegment(request.callback, segment);
+}
+
+function instrumentTedious(shim, tedious, _moduleName) {
 	shim.setDatastore(MSSQL);
 
 	const proto = tedious && tedious.Connection && tedious.Connection.prototype;
@@ -16,19 +29,6 @@ function instrumentTedious(shim, tedious, moduleName) {
 			callback: shim.FIRST
 		});
 	}
-}
-
-function tediousRequestQuery(shim, func, name, args) {
-	const request = args[0];
-	if (request.parametersByName && request.parametersByName.statement) {
-		return request.parametersByName.statement.value;
-	}
-	return request.sqlTextOrProcedure;
-}
-
-function tediousRequestCallback(shim, opFunc, opName, segment, args) {
-	const request = args[0];
-	request.callback = shim.bindSegment(request.callback, segment);
 }
 
 newrelic.instrumentDatastore('tedious', instrumentTedious);
